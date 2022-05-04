02ce0ccc146b9ba438e97636097e8d9a82f55dbb
ctlon <ctlon@DESKTOP-JEBTR5C>
2022-05-04 13:33:47 +0900

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+using Microsoft.MixedReality.WebRTC;
+using System.Diagnostics;
+using Windows.Media.Capture;
+using Windows.ApplicationModel;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Demo_WebRTC
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            Application.Current.Suspending += Current_Suspending;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Request access to microphone and camera
+            var settings = new MediaCaptureInitializationSettings();
+            settings.StreamingCaptureMode = StreamingCaptureMode.AudioAndVideo;
+            var capture = new MediaCapture();
+            await capture.InitializeAsync(settings);
+
+            // Retrieve a list of available video capture devices (webcam)
+            IReadOnlyList<VideoCaptureDevice> deviceList = 
+                await DeviceVideoTrackSource.GetCaptureDevicesAsync();
+
+            // Get the device list and, for example, print them to the debugger console
+            foreach (var device in deviceList)
+            {
+                // This message will show up in the Output window of Visual Studio
+                Debugger.Log(0, "", $"Webcam {device.name} (id : {device.id})\n");
+            }   
+
+        }
+
+        private void Current_Suspending(object sender, SuspendingEventArgs e)
+        {
+            
+        }
+    }
+}
